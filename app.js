c36040895c9594e5b57732dbb982571b00676961
Kelly <aisling.kelly@accenture.com>
2022-11-05 14:28:02 +0000

    Bug Fixes and icons

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-function formatDate(date) {
+//Get date and time
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -8,7 +10,6 @@ function formatDate(date) {
     minutes = `0${minutes}`;
   }
 
-  let dayIndex = date.getDay();
   let days = [
     "Sunday",
     "Monday",
@@ -18,34 +19,51 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-  let day = days[dayIndex];
-
+  let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayWeatherCondition(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#actual-temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#feels-like").innerHTML = Math.round(
-    response.data.main.feels_like
-  );
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+  return days[day];
 }
 
+//Get location and weather conditions
+function displayWeatherCondition(response) {
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#actual-temperature");
+  let humidityElement = document.querySelector("#humidity");
+  let realfeelElement = document.querySelector("#real-feel");
+  let windElement = document.querySelector("#wind");
+  let descriptionElement = document.querySelector("#description");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  humidityElement.innerHTML = response.data.main.humidity;
+  realfeelElement.innerHTML = Math.round(response.data.main.feels_like);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+
+//Get city
 function searchCity(city) {
   let apiKey = "79029da8c557ada5f3fa67fa4ac0ff29";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
+//Search for location
 function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
@@ -64,12 +82,15 @@ function getCurrentLocation(event) {
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
-// Display temperature in Celsius and in Fahrenheit
+//Get temperature in Celsius and in Fahrenheit
 function convertToFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#actual-temperature");
   temperatureElement.innerHTML = 62;
 }
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 function convertToCelsius(event) {
   event.preventDefault();
@@ -77,15 +98,8 @@ function convertToCelsius(event) {
   temperatureElement.innerHTML = 17;
 }
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
-
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
-
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-dateElement.innerHTML = formatDate(currentTime);
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
@@ -94,121 +108,3 @@ let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
 searchCity("Celbridge");
-
-/* 
-let apiKey = "79029da8c557ada5f3fa67fa4ac0ff29";
-let city = "new york";
-let units = "metric";
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=sydney&appid=79029da8c557ada5f3fa67fa4ac0ff29&units=metric";
-
-
-let h1 = document.querySelector("#city");
-h1.innerHTML = city;
-
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  oslo: {
-    temp: -5,
-    humidity: 20,
-  },
-};
-
-
-function formatDate(todaysDate) {
-  let minutes = todaysDate.getMinutes();
-  let hours = todaysDate.getHours();
-  let month = todaysDate.getMonth();
-  let year = todaysDate.getFullYear();
-  let currentDate = todaysDate.getDay();
-  let day = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let today = day[currentDate];
-  return `${today} ${hours}:${minutes} ${month} ${year}`;
-}
-
-function search(event) {
-  event.preventDefault();
-
-  let cityElement = document.querySelector("#city");
-  let cityInput = document.querySelector("#input");
-  cityElement.innerHTML = cityInput.value;
-  console.log(cityElement, cityInput.value);
-}
-
-function fahrenheit(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 66;
-}
-
-function celsius(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 19;
-}
-
-// Feature #1
-let todaysDate = document.querySelector("#todaysDate");
-let currentTime = new TodaysDate();
-todaysDate.innerHTML = formatDate(currentTime);
-
-// Feature #2
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
-
-
-
-// write your code here
-/*
-function getCity() {
-  let city = prompt("Please enter a city");
-  city.toLowerCase();
-  city.concat();
-
-  let temperature = weather[city].temp;
-  let humidity = weather[city].humidity;
-  let celsiusTemperature = Math.round(temperature);
-  let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
-
-  if (
-    city === "paris" ||
-    city === "tokyo" ||
-    city === "lisbon" ||
-    city === "sanfrancisco" ||
-    city === "oslo"
-  ) {
-    alert(
-      `It is currently ${celsiusTemperature}°C/${fahrenheitTemperature}°F in ${city} with a humidity of ${humidity}%`
-    );
-  } else {
-    alert(
-      "Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+sydney"
-    );
-  }
-}
-
-getCity();
-*/
