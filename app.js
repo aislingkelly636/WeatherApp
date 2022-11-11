278ffdbfebbf4614afa8b6bb0b89df979dea4dec
Kelly <aisling.kelly@accenture.com>
2022-11-11 16:20:46 +0000

    Display weekly forecast data using API

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+//Weekly forecast days
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
@@ -31,27 +32,38 @@ function formatDay(timestamp) {
   return days[day];
 }
 
-function displayForecast() {
+//Weekly forecase
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Thu", "Fri", "Sat", "Sun"];
 
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `            
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `            
               <div class="col-2">
-                <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-date">${formatDay(
+                  forecastDay.dt
+                )}</div>
                   <img
-                    src="http://openweathermap.org/img/wn/10d@2x.png"
+                    src="http://openweathermap.org/img/wn/${
+                      forecastDay.weather[0].icon
+                    }@2x.png"
                     alt="few clouds"
                     width="42"
                   />
                   <div class="weather-forecast-temperatures">
-                    <span class="weather-forecast-temperatures-max"> 18°</span>
-                    <span class="weather-forecast-temperatures-min"> 10°</span>
+                    <span class="weather-forecast-temperatures-max"> ${Math.round(
+                      forecastDay.temp.max
+                    )}°</span>
+                    <span class="weather-forecast-temperatures-min"> ${Math.round(
+                      forecastDay.temp.min
+                    )}°</span>
               </div>
             </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
